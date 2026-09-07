2ecda707e710030e745ce60160c103e614066e3c
towa0504100239-glitch <towa0504.100239@gmail.com>
2026-09-07 16:08:14 +0900

    add commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import Works from "./pages/Works";
 import TaskQuest from "./pages/works/TaskQuest";
 import FitBuddy from "./pages/works/FitBuddy";
 import ColorGame from "./pages/works/ColorGame";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/works" element={<Works />} />
-
         <Route path="/works/task-quest" element={<TaskQuest />} />
         <Route path="/works/fitbuddy" element={<FitBuddy />} />
         <Route path="/works/color-game" element={<ColorGame />} />
